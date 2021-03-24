8805c931185d0b2b6d89347b837e157447d86bc1
Jianing <jianingw321@outlook.com>
2021-03-24 20:47:29 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Learning Express
+
+with
+
+- nodemon
+- moment
+- express-handlebars
+- uuid
+- postman
+- bootsrap
+
+problem
+
+- postman 读取不到 req.body(但实际可以)
---
+++ middleware/logger.js
@@ -0,0 +1,16 @@
+// time
+const moment = require("moment");
+
+//middleware
+const logger = (req, res, next) => {
+  console.log("Hello");
+  console.log(
+    `${req.protocol}://${req.get("host")}${
+      req.originalUrl
+    }: ${moment().format()}`
+  );
+
+  next();
+};
+
+module.exports = logger;
